309058d699a37fceccaaaabb9f349ffe3f8a28c8
FrozyPenguin <33401597+FrozyPenguin@users.noreply.github.com>
2021-03-14 01:29:26 +0100

    Sauvegarde des deux versions de la map

--- js/groupes.local.js
+++ js/groupes.local.js
@@ -1,6 +1,6 @@
 const groupesLocaux = [
-    `/merge/groupes/Groupe-000000.geojson`,
-    `/merge/groupes/Groupe-00ff00.geojson`,
-    `/merge/groupes/Groupe-77B5FE.geojson`,
-    `/merge/groupes/Groupe-ff6C9E.geojson`
+    `/mergerTool/groupes/Groupe-000000.geojson`,
+    `/mergerTool/groupes/Groupe-00ff00.geojson`,
+    `/mergerTool/groupes/Groupe-77B5FE.geojson`,
+    `/mergerTool/groupes/Groupe-ff6C9E.geojson`
 ]

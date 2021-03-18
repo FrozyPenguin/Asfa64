02537387a2affff3d44073b9f3be95be2b0a3d76
Tison Florian <tisonflo@hotmail.fr>
2021-03-18 09:59:21 +0100

    Rétablissement du listing par ordre alphabétique

--- frontend/js/listing.js
+++ frontend/js/listing.js
@@ -3,46 +3,34 @@ import { addCityMarker, removeMarker } from './map.js';
 import { createElementFromHTML } from './utils/htmlToDom.js';
 
 function createListing() {
-    let villes = [];
     const toShow = ['Orthez', 'Artix', 'Arthez de Béarn', 'Puyoo', 'Mourenx', 'Monein'];
-    const listingAlphabeticGroup = document.querySelector('#alphaPlusGroupe');
-    // Trie par groupe et par ordre alphabetique
+
+    // Trie global par ordre alphabétique
+    let villes = [];
+    const listingAlphabeticOnly = document.querySelector('#listing').querySelector('ul');
+
     groupesInfos.forEach(groupe => {
-        groupe.villes.sort((a, b) => {
-            let nameA = a.nom.toUpperCase();
-            let nameB = b.nom.toUpperCase();
-            return (nameA < nameB) ? -1 : (nameA > nameB) ? 1 : 0;
-        })
-
-        let accordionItem = `
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="head${groupe.color}">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#target${groupe.color}" aria-expanded="false" aria-controls="target${groupe.color}">
-                    ${groupe.nom}
-                </button>
-            </h2>
-            <div id="target${groupe.color}" class="accordion-collapse collapse" aria-labelledby="head${groupe.color}" data-bs-parent="#alphaPlusGroupe">
-                <div class="accordion-body">
-                    <ul class="list-group listing">
-                    </ul>
-                </div>
-            </div>
-        </div>
-        `;
-
-        const accordionBody = listingAlphabeticGroup.appendChild(createElementFromHTML(accordionItem)).querySelector('.accordion-body ul');
-
         groupe.villes.forEach(ville => {
-            accordionBody.appendChild(createElementFromHTML(`<li class="list-group-item" city="${ville.nom}" cp="${ville.cp}">${ville.nom}</li>`))
-            .addEventListener('click', liClickEvent);
-
-            if(toShow.includes(ville.nom)) villes.push(ville);
+            villes.push(ville);
         })
     })
 
+    villes.sort((a, b) => {
+        let nameA = a.nom.toUpperCase();
+        let nameB = b.nom.toUpperCase();
+        return (nameA < nameB) ? -1 : (nameA > nameB) ? 1 : 0;
+    })
+
     villes.forEach(ville => {
-        addCityMarker(ville.nom, ville.cp)
-        .then(marker =>updateList(ville.nom, ville.cp, marker));
+        listingAlphabeticOnly.appendChild(
+            createElementFromHTML(`<li class="list-group-item" city="${ville.nom}" cp="${ville.cp}">${ville.nom}</li>`)
+        )
+        .addEventListener('click', liClickEvent);
+
+        if(toShow.includes(ville.nom)) {
+            addCityMarker(ville.nom, ville.cp)
+            .then(marker =>updateList(ville.nom, ville.cp, marker));
+        }
     })
 }
 
